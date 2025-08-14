a949f50c4db9d3b9b585be8525851deb2352c98a
kevintc42 <kevintriana41@gmail.com>
2025-08-14 09:58:09 -0500

    Carrusel hecho con base de datos

---
+++ backend/products/serializers.py
@@ -0,0 +1,8 @@
+# backend/products/serializers.py
+from rest_framework import serializers
+from .models import Product # Ajusta el nombre si tu modelo es distinto
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'nombre', 'precio', 'imagen']  # Ajusta los campos
