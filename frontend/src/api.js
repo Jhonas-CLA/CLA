29af7bb19ae36f43144acec70b80de6268a51246
Jhonas-CLA <jjgaitan4@gmail.com>
2025-09-28 15:11:15 -0500

    Feature/migracion (#76)
    * Commit final del proyecto
    
    * Commit final del proyecto
    
    * Migracion de sqlite3 a postgresql
    
    * Despliegue en render
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Despliegue react
    
    * Despliegue react

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // URL base de tu backend en Render
-export const BASE_URL = "https://electricosandsoluciones.onrender.com/api";
+export const BASE_URL = "https://electricosandsolucionesfrontend.onrender.com";
 
 // Instancia de axios para usar en toda la app
 const api = axios.create({
