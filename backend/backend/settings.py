38462c62bf0b164c3a8a3d920e057c0e23ed6101
Jhonas-CLA <jjgaitan4@gmail.com>
2025-09-28 15:41:56 -0500

    Feature/migracion (#80)
    * Commit final del proyecto
    
    * Commit final del proyecto
    
    * Migracion de sqlite3 a postgresql
    
    * Despliegue en render
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-d!3y1vgzp)ax)ist7+^ez^)z6wkjtf#l2on#h+75q9_hvt!87^'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["electricosandsoluciones.onrender.com"]
 
 # Aplicaciones instaladas
 INSTALLED_APPS = [
@@ -120,6 +120,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "CSRF_TRUSTED_ORIGINS",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
