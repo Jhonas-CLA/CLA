09599af5ae13cf253d16cef03eb5211513ad75c7
Jhonas-CLA <jjgaitan4@gmail.com>
2025-09-28 16:42:21 -0500

    Feature/migracion2 (#87)
    * Commit final del proyecto
    
    * Commit final del proyecto
    
    * Migracion de sqlite3 a postgresql
    
    * Despliegue en render
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Mas arreglos en el deploy
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react
    
    * Despliegue react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,3 +154,14 @@ SIMPLE_JWT = {
 
 # Campo por defecto en modelos
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.environ.get('CREATE_SUPERUSER') == 'true':
+    import django
+    django.setup()
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(is_superuser=True).exists():
+        User.objects.create_superuser(
+            email='codigolatino123@gmail.com', 
+            password='123456'
+        )
