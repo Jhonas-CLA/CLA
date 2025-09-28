472c322e88fb22879717911dde4e9684305da48d
Jhonas-CLA <jjgaitan4@gmail.com>
2025-09-28 17:22:36 -0500

    Despliegue react

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../api";
 import "./Home.css";
+import useNavigate from 'react-router-dom';
 import ProductCarousel from "../components/ProductCarousel";
 
 function Home() {
