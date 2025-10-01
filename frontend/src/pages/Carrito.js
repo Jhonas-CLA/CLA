c9b4d7251390b2ac094a4e86908e055fa551e5b7
Jhonas-CLA <jjgaitan4@gmail.com>
2025-09-30 20:32:32 -0500

    Despliegue del reset de contraseña

--- frontend/src/pages/Carrito.js
+++ frontend/src/pages/Carrito.js
@@ -921,23 +921,19 @@ const CarritoCompras = () => {
         </div>
 
         {/* carrito lateral */}
-        <div style={{ display: "flex", gap: "20px", alignItems: "flex-start" }} className="layout-principal">
-          <div
-            className="carrito-lateral"
-            style={{
-              width: "400px",
-              backgroundColor: "white",
-              borderRadius: "20px",
-              padding: "30px",
-              boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-              position: "sticky",
-              top: "20px",
-              maxHeight: "80vh",
-              overflowY: "auto",
-              order: 2,
-            }}
-          ></div>
-
+        <div
+          style={{
+            width: "400px",
+            backgroundColor: "white",
+            borderRadius: "20px",
+            padding: "30px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            position: "sticky",
+            top: "20px",
+            maxHeight: "80vh",
+            overflowY: "auto",
+          }}
+        >
           <h2
             style={{
               color: "#1e3a8a",
